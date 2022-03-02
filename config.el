2b025afab14cf6226f3466bfd4fc439c49d777d7
Ivan Chien <qyc027@gmail.com>
2022-03-02 17:48:32 +0800

    Update for cc-lsp +eglot

--- config.el
+++ config.el
@@ -55,10 +55,15 @@
 ;; they are implemented.
 
 
+;; evil escape mapping
 (setq-default evil-escape-key-sequence "kj")
 
+
+;; default fullscreen
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
+
+;; key bindings
 (map! :leader
       (:when (featurep! :ui tabs)
        (:prefix-map ("j" . "tabs")
@@ -86,7 +91,6 @@
 (map! :leader
       :desc "Disable highlight"              "0"       #'evil-ex-nohighlight)
 
-
 (add-hook 'with-editor-mode-hook
           (lambda ()
             (map! :leader
@@ -95,7 +99,25 @@
                     :desc "Cancel"   "k" #'with-editor-cancel)))))
 
 
+;; haskell-lsp
 (setq lsp-haskell-server-path "/Users/ivan/.local/bin/haskell-language-server-macOS-8.10.7")
 
 
+;; cc-lsp + eglot
+(setq-default lsp-clangd-binary-path "/etc/local/opt/llvm/bin/clangd")
+(setq-default lsp-clangd-version "13.0.1")
+
+(setq lsp-clients-clangd-args '("-j=3"
+				"--background-index"
+				"--clang-tidy"
+				"--completion-style=detailed"
+				"--header-insertion=never"
+				"--header-insertion-decorators=0"))
+
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
+(set-eglot-client! 'cc-mode '("clangd" "-j=3" "--clang-tidy"))
+
+
+;; wakatime mode
 (global-wakatime-mode)
