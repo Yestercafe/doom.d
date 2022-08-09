28a7c4356d0579fc548a1b2b3e66acbb70b802e7
Ivan Chien <qyc027@gmail.com>
2022-08-10 00:07:54 +0800

    feat: update `clangd` path and version

--- addons/+lsp-config.el
+++ addons/+lsp-config.el
@@ -1,8 +1,8 @@
 ;;; +lsp.el -*- lexical-binding: t; -*-
 
 ;; cc-lsp + eglot
-(setq-default lsp-clangd-binary-path "/etc/local/opt/llvm/bin/clangd")
-(setq-default lsp-clangd-version "13.0.1")
+(setq-default lsp-clangd-binary-path "/usr/local/opt/llvm/bin/clangd")
+(setq-default lsp-clangd-version "14.0.6")
 
 (setq lsp-clients-clangd-args '("-j=3"
 				"--background-index"
