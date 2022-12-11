ce14527505a8bb4e38e94d8032d0d86c26364be2
Ivan Chien <qyc027@gmail.com>
2022-12-12 00:52:34 +0800

    Update appearance

--- config.el
+++ config.el
@@ -22,12 +22,12 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Hack Nerd Font" :size 14))
+(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-material-dark)
+(setq doom-theme 'doom-monokai-ristretto)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
